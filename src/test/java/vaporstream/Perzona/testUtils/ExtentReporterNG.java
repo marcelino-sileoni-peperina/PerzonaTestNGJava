ddd6a878fb533c0bd4e93dace212bbd0cae4b044
Marcelino Sileoni <marcelino.sileoni@peperina.io>
2023-09-25 08:37:24 -0300

    Remove test and target directories

--- src/test/java/vaporstream/Perzona/testUtils/ExtentReporterNG.java
+++ src/test/java/vaporstream/Perzona/testUtils/ExtentReporterNG.java
@@ -6,15 +6,16 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class ExtentReporterNG {
 
 	public static ExtentReports getReporterObject() {
-		//		ExtentRepors & ExtentSparkReporter
-		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+		// ExtentRepors & ExtentSparkReporter
+		String path = System.getProperty("user.dir") + "\\reports\\index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
-		reporter.config().setReportName("Web Automation Reports");
+		reporter.config().setReportName("Perzona Automation Tests - Reports");
 		reporter.config().setDocumentTitle("Resultados de los Tests");
-		
+
 		ExtentReports extent = new ExtentReports();
 		extent.attachReporter(reporter);
 		extent.setSystemInfo("Tester", "Peperina QA");
+		extent.setSystemInfo("Any System Info", "Any value");
 		return extent;
 	}
 
